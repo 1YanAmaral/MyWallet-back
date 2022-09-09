48e02d1e1beffddc62dee9444a3db0d59bd99185
Yan Amaral <yan@pop-os.localdomain>
2022-09-09 10:01:52 -0300

    Post e get de transações

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import joi from "joi";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 dotenv.config();
 
 const app = express();
@@ -79,7 +80,95 @@ app.post("/sign-in", async (req, res) => {
       return res.status(404).send("Usuário ou senha não encontrada");
     }
 
-    return res.sendStatus(200);
+    const token = uuid();
+    await db.collection("sessions").insertOne({
+      userId: user._id,
+      token,
+    });
+
+    return res.send(token).status(200);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/credit", async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  const { amount, description } = req.body;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+      return res.sendStatus(401);
+    }
+
+    const user = await db.collection("users").findOne({ _id: session.userId });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    const newTransaction = await db.collection("transactions").insertOne({
+      amount,
+      description,
+      type: "credit",
+    });
+
+    res.send(newTransaction).status(201);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/debit", async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  const { amount, description } = req.body;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+      return res.sendStatus(401);
+    }
+
+    const user = await db.collection("users").findOne({ _id: session.userId });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    const newTransaction = await db.collection("transactions").insertOne({
+      amount,
+      description,
+      type: "debit",
+    });
+
+    res.send(newTransaction).status(201);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+      return res.sendStatus(401);
+    }
+
+    const transactions = await db.collection("transactions").find().toArray();
+
+    res.status(200).send(transactions);
   } catch (error) {
     console.log(error.message);
     res.sendStatus(500);
