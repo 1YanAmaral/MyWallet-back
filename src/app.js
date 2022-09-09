8e2e357f24029b1a000442ea65cdd45d0a036a32
Yan Amaral <yan@pop-os.localdomain>
2022-09-09 08:53:46 -0300

    Cadastro e login com validação

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import joi from "joi";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
@@ -16,7 +17,7 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("mywallet");
 });
 
-app.post("/cadastro", async (req, res) => {
+app.post("/sign-up", async (req, res) => {
   const { name, email, password, confirm_password } = req.body;
   const user = {
     name,
@@ -24,6 +25,8 @@ app.post("/cadastro", async (req, res) => {
     password,
     confirm_password,
   };
+
+  const passwordHash = bcrypt.hashSync(password, 10);
 
   const userSchema = joi.object({
     name: joi.string().required(),
@@ -40,10 +43,46 @@ app.post("/cadastro", async (req, res) => {
   }
 
   try {
-    await db.collections("users").insertOne(user);
+    const repeatedEmail = await db
+      .collection("users")
+      .find({ email: user.email })
+      .toArray();
+    if (repeatedEmail.length !== 0) {
+      return res.status(401).send("email já existe");
+    }
+
+    await db.collection("users").insertOne({
+      ...user,
+      password: passwordHash,
+      confirm_password: passwordHash,
+    });
     res.status(201).send("OK");
   } catch (error) {
-    res.send(error);
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.collection("users").findOne({ email });
+
+    if (!user) {
+      return res.status(404).send("Usuário ou senha não encontrada");
+    }
+
+    const isValid = bcrypt.compareSync(password, user.password);
+
+    if (!isValid) {
+      return res.status(404).send("Usuário ou senha não encontrada");
+    }
+
+    return res.sendStatus(200);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
   }
 });
 
